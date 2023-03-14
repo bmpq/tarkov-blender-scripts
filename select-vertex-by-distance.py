10f54c64cd4785dd5870d47301e4a86a524078d5
bmpq <73074311+bmpq@users.noreply.github.com>
2023-03-14 12:24:53 +0300

    vector operations are obviously faster

--- select-vertex-by-distance.py
+++ select-vertex-by-distance.py
@@ -1,11 +1,14 @@
 import bpy
-import math
 
 # useful to work with meshes that were edge split
 # (ripped from game engines)
 # basically works like 'select linked'
 # but selects vertices that are not actually linked
 # and only linked visually (are on the same coordinates)
+
+import time
+
+start = time.time()
 
 THRESHOLD = 0.0001
 
@@ -15,14 +18,10 @@ mesh = obj.data
 def get_selected_verts():
     return [v for v in mesh.vertices if v.select]
 
-def distance(t1, t2):
-    x1, y1, z1 = t1
-    x2, y2, z2 = t2
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
-    return distance
+def distance(v1, v2):
+    return (v2 - v1).length
 
 prev_selected_amount = len(get_selected_verts())
-
 while True:
     bpy.ops.object.mode_set(mode = 'OBJECT')
     for v in get_selected_verts():
@@ -43,3 +42,6 @@ while True:
     prev_selected_amount = len(get_selected_verts())
 
 bpy.ops.object.mode_set(mode = 'EDIT')
+
+end = time.time()
+print(format(end - start, '.3f') + 's')
