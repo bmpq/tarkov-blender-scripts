ba986cbf99ed5e210d4a0b3e1b1999d15b21a128
bmpq <73074311+bmpq@users.noreply.github.com>
2023-03-20 14:42:35 +0300

    Update select-vertex-by-distance_addon.py

--- select-vertex-by-distance_addon.py
+++ select-vertex-by-distance_addon.py
@@ -1,19 +1,21 @@
-import math
 import bpy
+import bmesh
+from mathutils import kdtree
+
+
 bl_info = {
-    "name": "Select Linked Vertices within Threshold",
-    "description": "Selects all linked vertices within a threshold distance of the currently selected vertices",
-    "author": "Your Name",
+    "name": "Select Visually Linked Vertices within Threshold",
+    "description": "Selects vertices within a threshold distance of the currently selected vertices. Basically selects vaisually connected vertices that are not actually connected",
+    "author": "bmpq",
     "version": (1, 0),
     "blender": (2, 93, 0),
-    "location": "View3D > Sidebar > Select Linked Vertices within Threshold",
+    "location": "View3D > Sidebar > Tools > Select Visually Linked Vertices within Threshold",
     "category": "Mesh",
 }
 
-
 class SelectLinkedVerticesWithinThreshold(bpy.types.Operator):
-    bl_idname = "mesh.select_linked_within_threshold"
-    bl_label = "Select Linked Vertices within Threshold"
+    bl_idname = "mesh.select_visually_linked_within_threshold"
+    bl_label = "Select Visually Linked Vertices within Threshold"
     bl_description = "Selects all linked vertices within a threshold distance of the currently selected vertices"
 
     threshold: bpy.props.FloatProperty(
@@ -27,43 +29,46 @@ class SelectLinkedVerticesWithinThreshold(bpy.types.Operator):
 
     def execute(self, context):
         obj = bpy.context.active_object
-        mesh = obj.data
+        me = obj.data
+        bm = bmesh.from_edit_mesh(me)
 
-        def get_selected_verts():
-            return [v for v in mesh.vertices if v.select]
+        size = len(bm.verts)
+        kd = kdtree.KDTree(size)
+        for i, vtx in enumerate(bm.verts):
+            kd.insert(vtx.co, i)
+        kd.balance()
 
-        def distance(t1, t2):
-            return (t2 - t1).length
+        changed = True
+        # Loop until no more matching vertices are found
+        while changed:
+            changed = False
 
-        prev_selected_amount = len(get_selected_verts())
-        while True:
-            bpy.ops.object.mode_set(mode='OBJECT')
-            for v in get_selected_verts():
-                for va in mesh.vertices:
-                    if va.select:
-                        continue
-                    if distance(v.co, va.co) > self.threshold:
-                        continue
-                    va.select = True
+            for v in bm.verts:
+                if not v.select:
+                    continue
 
-            bpy.ops.object.mode_set(mode='EDIT')
-            bpy.ops.mesh.select_linked()
+                for e in v.link_edges:
+                    if not e.select:
+                        e.select = True
+                        changed = True
 
-            if prev_selected_amount == len(get_selected_verts()):
-                break
+                for (co, index, dist) in kd.find_range(v.co, self.threshold):
+                    if not bm.verts[index].select:
+                        bm.verts[index].select = True
+                        changed = True
 
-            prev_selected_amount = len(get_selected_verts())
+        bm.select_flush(True)
+        bmesh.update_edit_mesh(me)
 
-        bpy.ops.object.mode_set(mode='EDIT')
         return {'FINISHED'}
 
 
 class SelectLinkedVerticesWithinThresholdPanel(bpy.types.Panel):
     bl_idname = "OBJECT_PT_select_linked_vertices_within_threshold"
-    bl_label = "Select Linked Vertices within Threshold"
+    bl_label = "Select visually Linked Vertices within Threshold"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_category = "Select"
+    bl_category = "Tools"
 
     def draw(self, context):
         layout = self.layout
@@ -71,10 +76,10 @@ class SelectLinkedVerticesWithinThresholdPanel(bpy.types.Panel):
         layout.use_property_decorate = False
 
         layout.label(
-            text="Select linked vertices within a threshold distance of currently selected vertices:")
+            text="Select visually linked vertices within a threshold distance of currently selected vertices:")
         layout.prop(context.scene, "threshold")
-        layout.operator("mesh.select_linked_within_threshold",
-                        text="Select Linked Vertices")
+        layout.operator("mesh.select_visually_linked_within_threshold",
+                        text="Select Visually Linked Vertices")
 
 
 classes = [
