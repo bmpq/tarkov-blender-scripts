fd697eacc550f743d9565a6381ce9fca8bfad03c
bmpq <73074311+bmpq@users.noreply.github.com>
2023-03-20 14:14:05 +0300

    bmesh and kdtree optimization
    it's >1000x faster (bruh)

--- select-vertex-by-distance.py
+++ select-vertex-by-distance.py
@@ -1,47 +1,43 @@
-import bpy
-
 # useful to work with meshes that were edge split
 # (ripped from game engines)
 # basically works like 'select linked'
 # but selects vertices that are not actually linked
 # and only linked visually (are on the same coordinates)
 
-import time
-
-start = time.time()
+import bpy
+import bmesh
+from mathutils import kdtree
 
 THRESHOLD = 0.0001
 
 obj = bpy.context.active_object
-mesh = obj.data
+me = obj.data
+bm = bmesh.from_edit_mesh(me)
 
-def get_selected_verts():
-    return [v for v in mesh.vertices if v.select]
+size = len(bm.verts)
+kd = kdtree.KDTree(size)
+for i, vtx in enumerate(bm.verts):
+    kd.insert(vtx.co, i)
+kd.balance()
 
-def distance(v1, v2):
-    return (v2 - v1).length
+changed = True
+# Loop until no more matching vertices are found
+while changed:
+    changed = False
 
-prev_selected_amount = len(get_selected_verts())
-while True:
-    bpy.ops.object.mode_set(mode = 'OBJECT')
-    for v in get_selected_verts():
-        for va in mesh.vertices:
-            if va.select:
-                continue
-            if distance(v.co, va.co) > THRESHOLD:
-                continue
-            va.select = True
+    for v in bm.verts:
+        if not v.select:
+            continue
 
-    bpy.ops.object.mode_set(mode = 'EDIT')
-    bpy.ops.mesh.select_linked()
+        for e in v.link_edges:
+            if not e.select:
+                e.select = True
+                changed = True
 
-    bpy.context.view_layer.update()
+        for (co, index, dist) in kd.find_range(v.co, THRESHOLD):
+            if not bm.verts[index].select:
+                bm.verts[index].select = True
+                changed = True
 
-    if prev_selected_amount == len(get_selected_verts()):
-        break
-    prev_selected_amount = len(get_selected_verts())
-
-bpy.ops.object.mode_set(mode = 'EDIT')
-
-end = time.time()
-print(format(end - start, '.3f') + 's')
+bm.select_flush(True)
+bmesh.update_edit_mesh(me)
